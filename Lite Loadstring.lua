3801c223db45954073b4e14e033f225212c17f80
zShadowSkilled <103582788+zShadowSkilled1@users.noreply.github.com>
2022-09-26 14:40:55 +0200

    Update Lite Loadstring.lua

--- Lite Loadstring.lua
+++ Lite Loadstring.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/zShadowSkilled1/Shadow-s-Hub/main/Lite", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/zShadowSkilled1/Shadow-s-Hub/main/Lite.lua", true))()
