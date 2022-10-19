22229a57282cd9ebc1f3ddd37779dac8313c869d
xShadow <103582788+zShadowSkilled1@users.noreply.github.com>
2022-10-19 13:40:01 +0200

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,1 +1,7 @@
+--Scripts :
+
+loadstring(game:HttpGet("https://app.shadowhub.xyz/loadstring.html", true))()
+
+--If this one is not working , try this script :
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/zShadowSkilled1/Shadow-s-Hub/main/loader.lua", true))()
